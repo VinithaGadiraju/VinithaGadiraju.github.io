32b143bb3f7f53d3f31061886137ad362afc37b2
Vinitha Gadiraju <vinithag@uoregon.edu>
2019-02-21 18:17:27 -0700

    upload css and js files

---
+++ csci5849-dresspuppy/puppy.js
@@ -0,0 +1,23 @@
+function flowerFunction() {
+	puppy_img.src = "puppy_flower.jpg"
+}
+
+function tieFunction() {
+	puppy_img.src = "puppy_tie.jpg"
+}
+
+function collarFunction() {
+	puppy_img.src = "puppy_collar.jpg"
+}
+
+function ballFunction() {
+	puppy_img.src = "puppy_ball.jpg"
+}
+
+function pantsFunction() {
+	puppy_img.src = "puppy_pants.jpg"
+}
+
+function boneFunction() {
+	puppy_img.src = "puppy_bone.jpg"
+}
